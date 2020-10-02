f795ee8c0bad805f0e9d49b00749d1c990f302ca
Laura Fernandes <lauramsfernandes@gmail.com>
2020-10-02 14:13:37 +0200

    Add rubric

---
+++ process_data.py
@@ -0,0 +1,18 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import sqlite3
+
+# Summary: read the dataset, clean the data, and then store it in a SQLite database
+
+# E - Extracting data
+# use pandas
+
+# T - Transforming data
+# use pandas
+
+# L - Loading data
+# use .to_sql() with SQLAlchemy engine
+
+
