0c3a84a468175b4d06aaaa1a57075e1bf86a343f
Laura Fernandes <lauramsfernandes@gmail.com>
2020-10-02 14:42:15 +0200

    Rubrics

---
+++ train_classifer.py
@@ -0,0 +1,6 @@
+# Split the data into train and test set
+
+# Create a ML pipeline with NLTK, scikit-learn's Pipeline and GridSearchCV
+# OUTPUT: a final model that uses 'message' column to predict classifications for 36 categories (multi-output classification)
+
+# Export model to a pickle file
