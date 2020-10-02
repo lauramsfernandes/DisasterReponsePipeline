ed5d1af48528092f5f0bb45a4398e948c3635357
Laura Fernandes <lauramsfernandes@gmail.com>
2020-10-02 20:26:07 +0200

    Run process_data.py

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DisasterResponsePipeline
+
 This is a project proposed by Udacity Data Science Course, and the main objective is to analyze disaster data from Figure Eight to build a model for an API that classifies disaster messages.
 
 ## Sumary
@@ -6,7 +7,7 @@ This is a project proposed by Udacity Data Science Course, and the main objectiv
 ## How to Run
 
 ### Python Scripts
-### Web App
-### Files
 
-##
+### Web App
+
+### Files
--- data/process_data.py
+++ data/process_data.py
@@ -2,17 +2,50 @@
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-import sqlite3
+from sqlalchemy import create_engine
 
 # Summary: read the dataset, clean the data, and then store it in a SQLite database
 
-# E - Extracting data
-# use pandas
+## E - Extracting data
 
-# T - Transforming data
-# use pandas
+# Load messages dataset
+messages = pd.read_csv(r'data\messages.csv')
+
+# Load categories dataset
+categories = pd.read_csv(r'data\categories.csv')
+
+## T - Transforming data
+
+# Merge messages and categories dataset into a new dataframe
+df = pd.merge(categories, messages)
+
+# Split `categories` into separate category columns
+categories = categories.loc[:,'categories'].str.split(pat=';', expand=True)
+
+# Select the first row of the categories dataframe in order to set the columns name
+row = categories.iloc[0].values
+
+# Clean the columns name removing the last two characters
+category_colnames = list(map(lambda name: name[slice(-2)], row))
+
+# Rename `categories` columns
+categories.columns = category_colnames
+
+# Convert category values to just numbers 0 or 1
+for column in categories:
+    # Set each value to be the last character of the string
+    categories[column] = categories[column].str.get(-1)
+    # Convert each value from string to numeric
+    categories[column] = pd.to_numeric(categories[column])
+
+# Replace `categories` column in df with new category columns
+df.drop('categories', axis=1, inplace=True)
+df = pd.concat([df,categories], axis=1)
+
+#Remove duplicates
+df.drop_duplicates(inplace=True)
 
 # L - Loading data
-# use .to_sql() with SQLAlchemy engine
-
+engine = create_engine('sqlite:///InsertDatabaseName.db')
+df.to_sql('InsertTableName', engine, index=False)
 
