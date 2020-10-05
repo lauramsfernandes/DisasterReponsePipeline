58272cef2d8f69fefbed10bfd93fc7e03d1cc4d7
Laura Fernandes <lauramsfernandes@gmail.com>
2020-10-05 17:02:02 +0200

    Ultimas mudancas

--- models/train_classifer.py
+++ models/train_classifer.py
@@ -1,3 +1,21 @@
+# Import libraries
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
 # Split the data into train and test set
 
 # Create a ML pipeline with NLTK, scikit-learn's Pipeline and GridSearchCV
